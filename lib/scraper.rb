e7038a1900b0ce3b18db5a9dfbd195edcbf05cc6
Amy Fong <amyhfong@gmail.com>
2020-02-14 20:46:31 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -9,6 +9,8 @@ class Scraper
   def get_page
     doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
 
+    doc.css(".post").each do |post|
+      course = Course.new
     doc.css(".post").first.css("h2").text
     doc.css(".post").first.css(".date").text
     doc.css(".post").first.css("p").text
